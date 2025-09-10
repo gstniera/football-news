00260e1b675fa59020641ef2d49471d12840b820
gstniera <gstniera@gmail.com>
2025-09-10 17:49:18 +0700

    Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gusti-niera-footballnews.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://gusti-niera-footballnews.pbp.cs.ui.ac.id/"
+]
 
 
 # Application definition
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
+            },
     },
 ]
 
