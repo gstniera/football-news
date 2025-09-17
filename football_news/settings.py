9df99ef1ac516f48bc838aadc9bc1fdc21843ac6
gstniera <gstniera@gmail.com>
2025-09-17 13:52:33 +0700

    Tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gusti-niera-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://gusti-niera-footballnews.pbp.cs.ui.ac.id/"
+    "https://gusti-niera-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
